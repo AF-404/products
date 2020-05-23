942e4e225fc8a09875bd740519cede0fdaaaa8d0
dilshan-98 <dinusha.dilshan.1.dd@gmail.com>
2020-05-23 21:44:04 +0530

    route done

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -12,6 +12,22 @@ const Home = () =>{
                 <h3 className="display-6 text-uppercase text-center py-3">
                     register
                 </h3>
+                <form>
+                    <div className="form-row mr-auto">
+                        <div className="col-md-6 mb-4">
+                            <input type="text" className="form-control" placeholder="First Name"/>
+                        </div>
+                        <div className="col-md-6 mb-4">
+                            <input type="text" className="form-control" placeholder="Last Name"/>
+                        </div>
+                        <div className="col-md-6 mb-4">
+                            <input type="text" className="form-control" placeholder="phone"/>
+                        </div>
+                        <div className="col-md-6 mb-4">
+                            <input type="text" className="form-control" placeholder=" email"/>
+                        </div>
+                    </div>
+                </form>
             </div>
         </div>
     );
